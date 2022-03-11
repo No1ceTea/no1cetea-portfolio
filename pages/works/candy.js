9299a628bea52419ece2513cc7d25b4bde220b4b
No1ceTea <71291683+No1ceTea@users.noreply.github.com>
2022-03-11 13:01:46 +0100

    Add candy.js

---
+++ pages/works/candy.js
@@ -0,0 +1,21 @@
+import NextLink from 'next/link'
+import {
+    Box,
+    Heading,
+    Text,
+    Container,
+    Divider,
+    Button,
+  } from '@chakra-ui/react'
+
+const candy = () => {
+    return(
+        <Box>
+
+        </Box>
+
+    )
+}
+
+export default candy
+export { getServerSideProps } from '../../components/chakra'
