191990d33d08c7ea2c0c1c4d74e8cf8af265022a
No1ceTea <71291683+No1ceTea@users.noreply.github.com>
2022-03-13 18:09:17 +0100

    Update candy.js

--- pages/works/candy.js
+++ pages/works/candy.js
@@ -1,21 +1,37 @@
-import NextLink from 'next/link'
-import {
-    Box,
-    Heading,
-    Text,
-    Container,
-    Divider,
-    Button,
-  } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
 
-const candy = () => {
-    return(
-        <Box>
+const Work = () => (
+  <Layout title="CandyShop">
+    <Container>
+      <Title>
+        CandyShop "Haribo" <Badge>2021</Badge>
+      </Title>
+      <P>
+        Site  créer pour mettre en pratique nos premières connaissance de première année. Nous avons ici utilisé principalement du PHP
+        pour créer des fonctionnalités tel que ajouter, modifier ou supprimer des bonbons de la base de données. 
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link href="#">
+            Link test <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>HTML, CSS, JS, PHP</span>
+        </ListItem>
+      </List>
 
-        </Box>
+      <WorkImage src="" alt="Candy00" />
+      <WorkImage src="" alt="Candy01" />
+    </Container>
+  </Layout>
+)
 
-    )
-}
-
-export default candy
+export default Work
 export { getServerSideProps } from '../../components/chakra'
