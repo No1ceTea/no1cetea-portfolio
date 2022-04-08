17e5412bb6180fa3a19ce109d6e3249853b251f0
No1ceTea <71291683+No1ceTea@users.noreply.github.com>
2022-04-08 11:44:45 +0200

    <Update formation.js fix deploy />

--- pages/formation.js
+++ pages/formation.js
@@ -1,4 +1,4 @@
-import { Container, Heading, List, ListIcon, ListItem, Text } from '@chakra-ui/react'
+import { Container, Heading, List, ListItem, Text } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section' 
 import P from '../components/paragraph'
