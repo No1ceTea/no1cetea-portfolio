6e22db71c205f67b46e767130bd0102b04609b62
No1ceTea <thaiwomtg@gmail.com>
2022-09-12 15:17:22 +0200

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -72,7 +72,7 @@ const Navbar = props => {
           mt={{ base: 4, md: 0 }}
         >
           <LinkItem href="/works" path={path} display="inline-flex" alignItems="center" style={{ gap:4 }} pl={2}>
-            <IoCode /> Projets
+            <IoCode /> Works
           </LinkItem>
           <LinkItem
             target="_blank"
